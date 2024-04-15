5492c20f7c4823486caefd58733a5b15537b48ef
Fabian <fabian@svift.studio>
2024-04-15 21:14:43 +0200

    big change

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
-let interval
-
-const element = document.querySelector('.scramble')
-const originalText = element.innerText
-
-const randomInt = max => Math.floor(Math.random() * max)
-const randomFromArray = array => array[randomInt(array.length)]
-
-const scrambleText = text => {
-  const chars = '*?><[]&@#)(.%$-_:/;?!'.split('')
-  return text
-    .split('')
-    .map(x => randomInt(3) > 1 ? randomFromArray(chars) : x)
-    .join('')
+function startTime() {
+  const today = new Date();
+  let h = today.getHours(); 
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s + " CEST";
+  setTimeout(startTime, 1000);
 }
-  
-element.addEventListener('mouseover', () => {
-  interval = setInterval(() =>
-    element.innerText = scrambleText(originalText)
-  , 60)
-})
-  
-element.addEventListener('mouseout', () => {
-  clearInterval(interval)
-  element.innerText = originalText
-})
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
